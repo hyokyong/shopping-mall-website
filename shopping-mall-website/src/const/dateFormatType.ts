f8005cf1b921474fce6585f2d8e55a5575b0b220
hyokyong <hyokyong76199@hanmail.net>
2024-06-24 14:28:50 +0900

    dateFormat, role 상수 추가

---
+++ shopping-mall-website/src/const/dateFormatType.ts
@@ -0,0 +1,8 @@
+enum DateFormatType {
+  FULL_DATE_WITH_TIME = 'YYYY-MM-DD HH:mm:ss',
+  FULL_DATE = 'YYYY-MM-DD',
+  DASH_YEAR_MONTH = 'YYYY-MM',
+  KO_YEAR_MONTH = 'YYYY년 MM월',
+}
+
+export default DateFormatType;
---
+++ shopping-mall-website/src/const/role.ts
@@ -0,0 +1,8 @@
+/** 전체 권한 */
+const ROLE = {
+  CUSTOMER: 'CUSTOMER',
+  ADMIN: 'ADMIN',
+  PARTNER: 'PARTNER',
+};
+
+export { ROLE };
